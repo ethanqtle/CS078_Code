6e706f9f7b8b2a128b12f4fa5e7d6ccb0d666436
Nguyen Le <lenguyen_d@hotmail.com>
2023-11-12 16:55:44 -0800

    Add Scheme calculator code

---
+++ chapter3/Practice_3_2.scm
@@ -0,0 +1,21 @@
+; Section 3.2.1
+
+(quotient 10 2)
+
+(+ (* 3 5) (- 10 6))
+
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+   (+ (- 10 7)
+      6))
+
+(>= 2 1)
+
+; Section 3.2.2
+(define m_pi 3.14)
+(* m_pi 2)
+
+(define (square x) (* x x))
+(square 21)
+(square (+ 2 5))
